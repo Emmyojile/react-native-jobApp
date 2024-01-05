83e0c75003d94db9c7b895fac5fbd907a694b716
Emmyojile <emmaojile99@gmail.com>
2024-01-05 15:23:09 +0100

    first commit

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Welcome TO Mobile Development Home Boy</Text>
+        </View>
+    );
+}
+
+export default Home;
